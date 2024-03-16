43b6efd05e1edfacc226f856a493826d03cb6804
hothugiang <21020309@vnu.edu.vn>
2024-03-17 00:06:44 +0700

    test

--- Test.py
+++ Test.py
@@ -1,27 +1,14 @@
 def transport_fee_cal(type, weight, distance):
     if type == 'D':
-        if 1 <= distance <= 50:
+        if 1 <= distance <= 500:
             fee = 10000
-        elif 50 < distance <= 500:
-            fee = 20000
         elif 500 < distance <= 1000:
-            fee = 30000
+            fee = 25000
     elif type == 'P':
-        # Tính phụ phí 
-        if weight <= 10:
-            e = 0
-        elif 10 < w <= 50:
-            e = 10000
-        else: 
-            e = 15000
-            
-        # Tính phí cơ bản dựa trên khoảng cách
-        if 1 <= distance <= 50:
-            fee = 15000 + e
-        elif 50 < distance<= 500:
-            fee = 25000 + e
+        if 1 <= distance <= 500:
+            fee = 15000 * weight
         elif 500 < distance <= 1000:
-            fee = 35000 + e
+            fee = 30000 * weight
 
     return fee
 
@@ -30,19 +17,15 @@ if not(t in ["D", "P"]):
     print("Invalid type")
     exit()
 
-w = int(input("Weight (kg): "))
-if type == 'D':
-    if not(0 <= w <= 1):
-        print("The weight of the document must be in the range 0 <= w <= 1")
-        exit()
-elif type == "P":
-    if not(0 < w <= 100):
-        print("The weight of the package must be in the range 0 < w <= 100")
+if type == "P":
+    w = int(input("Weight (kg): "))
+    if not(0 < w <= 10):
+        print("The weight must be in the range 0 < w <= 10")
         exit()
 
 d = int(input("Distance (km): "))
 if not(1 <= d <= 1000):
-    print("The transport distance must be in the range 1 < w <= 1000")
+    print("The transport distance must be in the range 1 <= w <= 1000")
     exit()
 
 transport_fee = transport_fee_cal(t, w, d)
