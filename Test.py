84ce6128ffcc55696fd1b511db391f5a01388750
hothugiang <21020309@vnu.edu.vn>
2024-03-22 09:54:25 +0700

    test

--- Test.py
+++ Test.py
@@ -1,38 +1,35 @@
-def transport_fee_cal(type, weight, distance):
-    if type == 'D':
-        if 1 <= distance < 500:
-            fee = 10000
-        elif 500 <= distance <= 1000:
-            fee = 25000
-    elif type == 'P':
-        if 1 <= distance < 500:
-            fee = 15000 * weight
-        elif 500 <= distance <= 1000:
-            fee = 30000 * weight
-
-    return fee
-
 def main():
     t = input("Type (D/P): ")
     if not(t in ["D", "P"]):
         print("Invalid type")
-        exit()
+        return
 
     if t == "P":
         w = float(input("Weight (kg): "))
         if not(0 < w <= 10):
             print("The weight must be in the range 0 < w <= 10")
-            exit()
+            return
     else:
         w = 0
 
     d = float(input("Distance (km): "))
     if not(1 <= d <= 1000):
         print("The distance must be in the range 1 <= w <= 1000")
-        exit()
+        return
 
-    transport_fee = transport_fee_cal(t, w, d)
-    print(f"Your transport fee is: {transport_fee} VND")
+    fee = 0
+    if t == 'D':
+        if 1 <= d < 500:
+            fee = 10000
+        elif 500 <= d <= 1000:
+            fee = 25000
+    elif t == 'P':
+        if 1 <= d < 500:
+            fee = 15000 * w
+        elif 500 <= d <= 1000:
+            fee = 30000 * w
+
+    print(f"Your transport fee is: {fee} VND")
 
 if __name__ == "__main__":
     main()
