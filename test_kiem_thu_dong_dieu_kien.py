9524b769899c775bcd8f99798e87cf376f04fef5
hothugiang <21020309@vnu.edu.vn>
2024-04-02 00:29:37 +0700

    Test kiem thu dong dieu kien

--- test_kiem_thu_dong_dieu_kien.py
+++ test_kiem_thu_dong_dieu_kien.py
@@ -13,37 +13,37 @@ class TestTransportFee(unittest.TestCase):
     @patch('builtins.print')
     def test_2(self, mock_print, mock_input):
         main()
-        mock_print.assert_called_with("The weight must be in the range 0 < w <= 10")
+        mock_print.assert_called_with("The weight must be in the range 0 ≤ w ≤ 10")
     
-    @patch('builtins.input', side_effect=['P', '1', '1500'])
+    @patch('builtins.input', side_effect=['P', '1', '2000'])
     @patch('builtins.print')
     def test_3(self, mock_print, mock_input):
         main()
-        mock_print.assert_called_with("The distance must be in the range 1 <= d <= 1000")
+        mock_print.assert_called_with("The distance must be in the range 1 ≤ d ≤ 1650")
 
-    @patch('builtins.input', side_effect=['D', '1'])
+    @patch('builtins.input', side_effect=['D', '10', '1'])
     @patch('builtins.print')
     def test_4(self, mock_print, mock_input):
         main()
-        mock_print.assert_called_with(10000)
+        mock_print.assert_called_with(150000)
 
-    @patch('builtins.input', side_effect=['D', '500'])
+    @patch('builtins.input', side_effect=['D', '1', '500'])
     @patch('builtins.print')
     def test_5(self, mock_print, mock_input):
         main()
         mock_print.assert_called_with(25000)
 
-    @patch('builtins.input', side_effect=['P', '1', '1'])
+    @patch('builtins.input', side_effect=['P', '0.1', '1'])
     @patch('builtins.print')
     def test_6(self, mock_print, mock_input):
         main()
-        mock_print.assert_called_with(15000.0)
+        mock_print.assert_called_with(10500)
 
     @patch('builtins.input', side_effect=['P', '1', '500'])
     @patch('builtins.print')
     def test_7(self, mock_print, mock_input):
         main()
-        mock_print.assert_called_with(30000.0)
+        mock_print.assert_called_with(30000)
 
 if __name__ == '__main__':
     unittest.main()
